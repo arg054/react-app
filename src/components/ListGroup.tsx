979c0acc8ddfc94781451499e787b2a8a9ba01ac
arg054 <maxim.golovenkov@gmail.com>
2024-08-06 10:45:17 +0200

    functions, constants and onClick events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,30 @@
 function ListGroup() {
+  let items = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+
+  //message using const
+  const message = items.length === 0 && <p>No items to display</p>;
+
+  //message using a function
+  const getMessage = () => {
+    return items.length === 0 && <p>No items to display</p>;
+  };
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Group</h1>
+      {message}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item + " clicked " + index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
